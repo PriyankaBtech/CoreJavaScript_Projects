932a7c9a2d32a1dfcfeed7cd8ac7f8c3a569a12b
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-10-20 23:24:11 +0530

    add delete operation

--- 06_CRUD/script.js
+++ 06_CRUD/script.js
@@ -1,7 +1,27 @@
+// Create operation
+
 let addBtn = document.getElementById('add-btn')
 addBtn.addEventListener('click', (e) => { // function for add value
-    console.log(e)
+    //console.log(e)
     let currentBtn = e.currentTarget
-    let currentInput = currentBtn.previousElementSibling
-    console.log(currentInput.value)
+    let currentInput = currentBtn.previousElementSibling // grap the input value
+    let currentList = currentInput.value
+
+    let createList = document.createElement('li')
+    //createList.classList.add('list-group-item')
+    createList.className = 'list-group-item d-flex justify-content-between'
+    createList.innerHTML = `<h3>${currentList}</h3><div>
+                        <button class="btn btn-edit">Edit</button>
+                        <button class="btn btn-delete" onclick="removeTopic(this)">Delete</button>
+                        </div>`
+
+    let topicList = document.getElementById('topic-list')
+    topicList.appendChild(createList)
 })
+
+// Delete operation
+
+function removeTopic(currElement) {
+    currElement.parentElement.remove()
+    
+}
