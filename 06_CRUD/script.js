f5311b4092d7ca4dfe0f0ee4e9d1c9d280bcae30
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-10-21 23:23:25 +0530

    add update operation

--- 06_CRUD/script.js
+++ 06_CRUD/script.js
@@ -1,27 +1,47 @@
-// Create operation
+// CRUD
 
+//# Create operation
 let addBtn = document.getElementById('add-btn')
+let topicList = document.getElementById('topic-list') // add in global, for give excess mutltiple place
+
 addBtn.addEventListener('click', (e) => { // function for add value
+
+    if(topicList.children[0].className = "emptyMsg") {
+        topicList.children[0].remove()
+    }
+
     //console.log(e)
-    let currentBtn = e.currentTarget
-    let currentInput = currentBtn.previousElementSibling // grap the input value
+    let currentBtn = e.currentTarget  
+    let currentInput = currentBtn.previousElementSibling // grap the input value 
     let currentList = currentInput.value
+    
 
     let createList = document.createElement('li')
     //createList.classList.add('list-group-item')
     createList.className = 'list-group-item d-flex justify-content-between'
     createList.innerHTML = `<h3>${currentList}</h3><div>
-                        <button class="btn btn-edit">Edit</button>
+                        <button class="btn btn-edit" onclick="editTopic(this)">Edit</button>
                         <button class="btn btn-delete" onclick="removeTopic(this)">Delete</button>
                         </div>`
 
-    let topicList = document.getElementById('topic-list')
+    //let topicList = document.getElementById('topic-list')
     topicList.appendChild(createList)
 })
 
-// Delete operation
-
+//# Delete operation
 function removeTopic(currElement) {
-    currElement.parentElement.remove()
+    currElement.parentElement.parentElement.remove()
     
+    //condition to give msg
+    if(topicList.children.length <= 0) {
+        let emptyMsg = document.createElement("h4")
+        emptyMsg.classList.add("emptyMsg")
+        emptyMsg.textContent = "No topic present, please add new topic"
+        topicList.appendChild(emptyMsg)
+    }
+}
+
+//# Update operation
+function editTopic(currElement) {
+    console.log(currElement.previousElementSibling)
 }
