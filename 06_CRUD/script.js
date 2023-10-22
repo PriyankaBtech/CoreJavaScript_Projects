373bfb776b66fc99574165c67c99f9d74b9aa1e6
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-10-22 16:22:50 +0530

    crud operation in js

--- 06_CRUD/script.js
+++ 06_CRUD/script.js
@@ -2,7 +2,7 @@
 
 //# Create operation
 let addBtn = document.getElementById('add-btn')
-let topicList = document.getElementById('topic-list') // add in global, for give excess mutltiple place
+let topicList = document.getElementById('topic-list') // add in global, for give excess to mutltiple place
 
 addBtn.addEventListener('click', (e) => { // function for add value
 
@@ -20,8 +20,8 @@ addBtn.addEventListener('click', (e) => { // function for add value
     //createList.classList.add('list-group-item')
     createList.className = 'list-group-item d-flex justify-content-between'
     createList.innerHTML = `<h3>${currentList}</h3><div>
-                        <button class="btn btn-edit" onclick="editTopic(this)">Edit</button>
-                        <button class="btn btn-delete" onclick="removeTopic(this)">Delete</button>
+                        <button class="btn btn-edit bg-primary text-white" onclick="editTopic(this)">Edit</button>
+                        <button class="btn btn-delete bg-danger text-white" onclick="removeTopic(this)">Delete</button>
                         </div>`
 
     //let topicList = document.getElementById('topic-list')
@@ -37,11 +37,35 @@ function removeTopic(currElement) {
         let emptyMsg = document.createElement("h4")
         emptyMsg.classList.add("emptyMsg")
         emptyMsg.textContent = "No topic present, please add new topic"
+        emptyMsg.style.color = "white"
         topicList.appendChild(emptyMsg)
     }
 }
 
 //# Update operation
 function editTopic(currElement) {
-    console.log(currElement.previousElementSibling)
+    //console.log(currElement.parentElement.previousElementSibling) // h3
+
+    if(currElement.textContent === "Done") {
+        currElement.textContent = "Edit"
+        let currTopicName = currElement.parentElement.previousElementSibling.value
+        // create heading
+        let currHeading = document.createElement('h4')
+        currHeading.textContent = currTopicName
+        currElement.parentElement.parentElement.replaceChild(currHeading, currElement.parentElement.previousElementSibling)
+
+    } else {
+        currElement.textContent = "Done"
+        let currTopicName = currElement.parentElement.previousElementSibling.textContent
+        // create input
+        let currentInput = document.createElement('input')
+        currentInput.type = "text"
+        currentInput.placeholder = "write topic"
+        currentInput.className = "update-input"
+        currentInput.value = currTopicName
+        console.log(currentInput)
+    
+       currElement.parentElement.parentElement.replaceChild(currentInput, currElement.parentElement.previousElementSibling)       
+    }
+  
 }
