23c783a4abdfdbbe9c9ff67d95686e9119a70e9e
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-12-09 22:43:56 +0530

    Random images using api

---
+++ 13_RandomImage/script.js
@@ -0,0 +1,22 @@
+document.getElementById('btn').addEventListener('click', getImages)
+
+async function getImages() {
+  const contentDiv = document.querySelector('.content')
+  contentDiv.innerHTML = '' // Clear existing images
+
+  try {
+    const response = await fetch('https://picsum.photos/v2/list')
+    const data = await response.json();
+
+    data.forEach(image => {
+      const imgElement = document.createElement('img')
+      imgElement.src = image.download_url
+      console.log(imgElement)
+
+      contentDiv.appendChild(imgElement)
+    })
+  } catch (error) {
+    console.error('Error fetching images:', error)
+  }
+  
+}
