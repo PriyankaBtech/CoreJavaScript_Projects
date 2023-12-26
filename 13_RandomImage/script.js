afcd49ea20bcb94bb6905717f78791f1ae015f59
PriyankaBtech <officialpriyanka2000@gmail.com>
2023-12-26 22:53:45 +0530

    Change style

--- 13_RandomImage/script.js
+++ 13_RandomImage/script.js
@@ -7,13 +7,15 @@ async function getImages() {
   try {
     const response = await fetch('https://picsum.photos/v2/list')
     const data = await response.json();
+    //console.log(data)
 
     data.forEach(image => {
       const imgElement = document.createElement('img')
       imgElement.src = image.download_url
-      console.log(imgElement)
-
+      //console.log(imgElement)
+      
       contentDiv.appendChild(imgElement)
+
     })
   } catch (error) {
     console.error('Error fetching images:', error)
--- 14_RandomJokes/script.js
+++ 14_RandomJokes/script.js
@@ -4,21 +4,25 @@ async function getChuckNorrisJoke() {
   const displayJoke = document.getElementById('display-joke')
   const jokeButton = document.getElementById('getJoke')
 
-  try {
-    // Disable the button while fetching
-    jokeButton.disabled = true
+    try {
+      // Disable the button while fetching
+      jokeButton.disabled = true
 
-    const response = await fetch('https://api.chucknorris.io/jokes/random')
-    const data = await response.json()
+      const response = await fetch('https://api.chucknorris.io/jokes/random')
+      const data = await response.json()
 
-    displayJoke.textContent = data.value
+      displayJoke.textContent = data.value
+      
+      setTimeout(() => {
+        jokeButton.textContent = "Next"
+      }, 100)        
 
-  } catch (error) {
-    console.error('Error fetching Chuck Norris joke:', error)
-    displayJoke.textContent = 'Oops! Something went wrong. Please try again.'
+    } catch (error) {
+      console.error('Error fetching Chuck Norris joke:', error)
+      displayJoke.textContent = 'Oops! Something went wrong. Please try again.'
 
-  } finally {
-    // Enable the button after fetching
-    jokeButton.disabled = false
+    } finally {
+      // Enable the button after fetching
+      jokeButton.disabled = false
+    }
   }
-}
